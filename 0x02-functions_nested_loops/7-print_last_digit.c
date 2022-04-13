0381f5d45916aca883fd0a6ae92df6e5885265ab
meyota <meyota08@gmail.com>
2022-04-13 09:39:56 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,11 @@
 /**
  * print_last_digit - prints the last digit of number
  *
- *@n: The number in qustion
+ * @n: The number in qustion
  * Return: value the last of digit
  */
 
-int print_last_digit (int n)
+int print_last_digit(int n)
 {
 int last_digit = n % 10;
 if (last_digit < 0)
