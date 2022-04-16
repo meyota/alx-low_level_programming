9d8b1fcdcc375afd1f757517aec0a7ffbdf45d2e
meyota <meyota08@gmail.com>
2022-04-16 01:57:42 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - tests a function if the integer is positive or negative
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
