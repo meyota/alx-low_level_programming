f6862b775d33b43eb61ca9124d99ed31087b1935
meyota <meyota08@gmail.com>
2022-04-16 03:41:47 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+i = 0;
+for (i = 0; i < 10; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
