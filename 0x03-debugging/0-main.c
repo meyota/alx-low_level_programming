0025ee1597a0d6a726d781fe5e88d6eab1a5a406
meyota <meyota08@gmail.com>
2022-04-16 02:09:10 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,13 +3,12 @@
 /**
  * main - tests a function if the integer is positive or negative
  *
- * Return: Always 0.
+ * Return: zero
  */
 
 int main(void)
 {
 int i;
-
 i = 0;
 positive_or_negative(i);
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 
 #include <stdio.h>
-void positive_or_negative(i);
+void positive_or_negative(int i);
 #endif
