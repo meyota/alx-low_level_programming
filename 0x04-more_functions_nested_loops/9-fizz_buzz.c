3aa590fa1a111f28821f2e31e69cba3448f102df
meyota <meyota08@gmail.com>
2022-04-16 08:07:34 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,13 @@ if (i % 3 == 0 && i % 5 != 0)
 printf("Fizz");
 }
 else if (i % 3 != 0 && i % 5 == 0)
+{
 printf("Buzz");
+}
 else if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz");
+}
 else
 {
 printf(%d, i);
