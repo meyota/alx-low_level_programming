2825c5e045e5d1fafed9ce5886c4f401cda4aee1
meyota <meyota08@gmail.com>
2022-04-21 09:47:00 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,13 @@
 char *_strcat(char *dest, char *src)
 {
 int i, j;
+
 i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
+
 j = 0;
 while (src[j] != '\0')
 {
@@ -22,5 +24,6 @@ j++;
 i++;
 }
 dest[i] = '\0';
+
 return (dest);
 }
