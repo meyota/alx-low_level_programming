30f9939567171779d2ed0dcf490259766eb33737
meyota <meyota08@gmail.com>
2022-04-23 14:53:06 -0700

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+int divisor = 1, i, z;
 
 if (n < 0)
 {
@@ -18,11 +18,10 @@ n *= -1;
 
 for (i = 0; n / divisor > 9; i++, divisor *= 10)
 ;
-
 for (; divisor >= 1; n %= divisor, divisor /= 10)
 {
-resp = n / divisor;
-_putchar('0' + resp);
+z = n / divisor;
+_putchar('0' + z);
 
 }
 
