d3d5361b74eac701c08b955a4adc48a13cff0128
meyota <meyota08@gmail.com>
2022-04-23 09:18:41 -0700

    Task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+int divisor = 1, y, z;
+
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+
+for (y = 0; n / divisor > 9; y++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+z = n / divisor;
+_putchar('0' + z);
+
+}
+
+}
