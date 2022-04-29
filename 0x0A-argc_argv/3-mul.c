df58a9d5ec700f68780a6be6b39080d4352dfc49
meyota <meyota08@gmail.com>
2022-04-29 11:18:10 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,17 @@
  **/
 int main(int argc, char *argv[])
 {
-int x, y;
+int x = 0, y = 0;
 if (argc == 3)
 {
 x = atoi(argv[1]);
 y = atoi(argv[2]);
+
 printf("%d\n", x *y);
 }
+
 else
+
 {
 printf("Error\n");
 return (1);
