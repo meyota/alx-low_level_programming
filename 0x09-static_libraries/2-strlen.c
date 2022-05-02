bb6b7a52c60eeb12667ca96949dc974a203b60a5
meyota <meyota08@gmail.com>
2022-05-02 00:59:19 -0700

    printf

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: A pointer to an int that will be updated
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+int x;
+
+x = 0;
+while (s[x] != '\0')
+{
+x++;
+}
+return (x);
+}
