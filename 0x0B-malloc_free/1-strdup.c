ac7fc890ed16c330bf1ac71cfc3bc2dbbd26db97
meyota <meyota08@gmail.com>
2022-05-02 10:00:46 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,17 +11,18 @@ char *_strdup(char *str)
 char *s;
 unsigned int i, j;
 
-if (s == NULL)
+if (str == NULL)
 	return (NULL);
 
-		for (i = 0; s[i] != '\0'; i++)
+		for (i = 0; str[i] != '\0'; i++)
 			s = (char *)malloc(sizeof(char) * (i +1));
 
 if (s == NULL)
 	return (NULL);
 
 for (j = 0; j <= i; j++)
-	s[j] = s[i];
+	s[j] = str[j];
+
 return (s);
 
 }
