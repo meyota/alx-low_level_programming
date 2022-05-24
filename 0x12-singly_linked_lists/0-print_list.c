df721c8cbc297f447ebf6d9cdb5f9823afee53b1
meyota <meyota08@gmail.com>
2022-05-24 10:59:24 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,15 @@
 size_t print_list(const list_t *h)
 {
 size_t numb_nodes;
-	numb_nodes = 0;
+numb_nodes = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-       printf("[%d] %s\n", 0, "(nil)");
+printf("[%d] %s\n", 0, "(nil)");
 else
-	printf("[%d] %s\n", h->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-	numb_nodes++;
+numb_nodes++;
 }
 return (numb_nodes);
 }
