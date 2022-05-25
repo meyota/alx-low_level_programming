445077991647c8344bca9b35d1bfa3990208eb5d
meyota <meyota08@gmail.com>
2022-05-25 08:57:39 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,6 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the start of the list
- *
  * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
@@ -19,11 +18,11 @@ while (h != NULL)
 
 {
 if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
+	printf("[%d] %s\n", 0, "(nil)");
 else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-numb_nodes++;
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	numb_nodes++;
 }
 
 return (numb_nodes);
