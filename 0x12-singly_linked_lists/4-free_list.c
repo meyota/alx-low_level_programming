55f3066fbf9b7a97ec65d927ac01e52122b9b650
meyota <meyota08@gmail.com>
2022-05-25 08:17:11 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,10 +11,12 @@
 void free_list(list_t *head)
 {
 list_t *current;
-while((current = head) != NULL)
-{
-head = head->next;
-free(current->str);
-free(Current);
-}
+
+while ((current = head) != NULL)
+	{
+
+	head = head->next;
+	free(current->str);
+	free(Current);
+	}
 }
