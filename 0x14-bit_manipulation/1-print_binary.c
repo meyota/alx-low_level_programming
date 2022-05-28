81bbb02e20f6fefe68f6fa3931b1d33fb2c4f35f
meyota <meyota08@gmail.com>
2022-05-28 02:54:43 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+unsigned long int deci;
+	if (n == 0)
+	{
+		_putchar('0');
+	
+	}
+	else
+	{
+		deci = n & 1;
+		n = n >> 1;
+		if (!(n == 0))
+			print_binary(n);
+		_putchar(deci + '0');
+	}
+}
+
