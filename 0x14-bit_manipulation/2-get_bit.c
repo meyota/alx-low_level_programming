59b053ab402ea51b51d2f02a029c4defc5ce0cf0
meyota <meyota08@gmail.com>
2022-05-28 03:16:11 -0700

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_index;
-	if (index > 64)
-		return (-1);
-	bit_index = (n >> index) & 1;
-	return (bit_index);
+int bit_index;
+if (index > 64)
+return (-1);
+bit_index = (n >> index) & 1;
+return (bit_index);
 }
