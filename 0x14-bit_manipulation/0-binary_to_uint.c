4cf016723b0a0a65f099b8e4119e5b160f48f6d8
meyota <meyota08@gmail.com>
2022-05-28 01:57:22 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,13 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int val = 0;
 int i;
- if (!b)
-	 return (0);
- for (i = 0; b[i]; i++)
- {
- if (b[i] < '0' || b[i] > '1')
-	 return (0);
- val = 2 * val + (b[i] - '0');
- }
- return (val);
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+val = 2 * val + (b[i] - '0');
+}
+return (val);
 }
