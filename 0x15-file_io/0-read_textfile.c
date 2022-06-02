550effeee2e568839d0b1289517118a5c58f2524
meyota <meyota08@gmail.com>
2022-06-02 06:21:05 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,31 +2,33 @@
 
 /**
  * read_textfile - reads a text file and prints it to the
- * 		   POSIX standard output
+ * 		POSIX standard output
  * @filename: pointer to text in a file
  * @letters: number of letters
  * Return: actual number of letters read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t file, fread, fwrite;
-char *buffer;
-buffer = malloc(sizeof(char) * letters);
-if (filename == NULL || buffer == NULL)
-{
-free(buffer);
-	return (0);
-}
-file = open(filename, O_RDONLY);
-if (file == -1)
-	return (0);
-fread = read(file, buffer, letters);
-if (fread == -1)
-	return (0);
-fwrite = write(STDOUT_FILENO, buffer, fread);
-if (fwrite == -1)
-	return (0);
-close(file);
-free(buffer);
-return(fwrite);
+	ssize_t file, fread, fwrite;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (filename == NULL || buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	fread = read(file, buffer, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, buffer, fread);
+	if (fwrite == -1)
+		return (0);
+	close(file);
+	free(buffer);
+	return(fwrite);
 }
