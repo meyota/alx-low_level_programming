8261537a5280ccfa8abbc57c4e3073f3a6ddb792
meyota <meyota08@gmail.com>
2022-06-03 04:31:21 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 int file, fwrite, k;
 	if (filename == NULL)
 		return (-1);
-file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (file == -1)
 	return (-1);
 if (text_content != NULL)
