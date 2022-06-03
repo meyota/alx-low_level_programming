1bef84e18c02e062b2ae093c8e8b6e6392d48370
meyota <meyota08@gmail.com>
2022-06-03 04:53:50 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int file, fwrite, k;
-	if (filename == NULL)
+int file, fwrite, j;
+	if (!filename)
 		return (-1);
-file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (file == -1)
 	return (-1);
 if (text_content != NULL)
 {
-for (i = 0; text_content[k]; k++)
+for (j = 0; text_content[j]; j++)
 	;
-fwrite = write(file, text_content, k);
+fwrite = write(file, text_content, j);
 if (fwrite == -1)
 	return (-1);
 }
