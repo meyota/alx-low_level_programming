2a84b4114e1e56db9b9cebea9e25568288223f0f
meyota <meyota08@gmail.com>
2022-07-07 11:27:19 -0700

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,14 +11,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t *current = head;
-unsigned int count = 0;
-while (current != NULL)
+	dlistint_t *current = head;
+	unsigned int count = 0;
+	while (current != NULL)
 	{
-	if (count == index)
-		return (current);
-	count++;
-	current = current->next;
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
 	}
-
+	return (NULL);
 }
