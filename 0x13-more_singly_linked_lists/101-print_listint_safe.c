932eb594db651ffc50ab9d9e8c0c95626dcbc1a5
meyota <meyota08@gmail.com>
2022-07-09 05:48:32 -0700

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -19,8 +19,10 @@ size_t print_listint_safe(const listint_t *head)
 		if (diff > 0)
 		head = head->next;
 		else
+		{
 		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 		break;
+		}
 	}
 
 	return (num);
