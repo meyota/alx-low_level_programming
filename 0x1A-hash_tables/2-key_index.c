e012bbe82b7ed8f9707ab6e588f01066d5982fa3
meyota <meyota08@gmail.com>
2022-07-30 02:21:49 -0700

    Task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - generates the key using hash_djb2
+ * @key: The key to get the index of 
+ * @size: The size of the array of hash table
+ * Return: index key
+ * Description: uses the djb2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
