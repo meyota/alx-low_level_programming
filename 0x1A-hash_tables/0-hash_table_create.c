94ba5953caf3f9ceeb436131b391c6dd6dbecebe
meyota <meyota08@gmail.com>
2022-08-03 06:54:16 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
-
 	if (table->array == NULL)
 	{
 		free(table);
